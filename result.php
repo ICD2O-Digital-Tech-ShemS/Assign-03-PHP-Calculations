0968510b33453ddaa27151855a59aa34d3ccfabc
Shem Irekpita <Shem.irekpita@stu.ocsb.ca>
2025-04-25 15:20:15 +0000

    This is my project

--- result.php
+++ result.php
@@ -5,9 +5,9 @@
 	$X2int = $_POST['X2int'];
 
 	$slope = ($Y2int - $Y1int)/($X2int - $X1int);
-	$calculation = $Y2int - (slope * X2int)
+	$calculation = $Y2int - ($slope * $X2int)
 	$calculation = round($calculation;1)
 ?>
 <h3>Results:</h3>
 Your slope is <?php echo "$slope" ?>
-Your Yint is <?php echo "$calculation" ?>
+Your y-int is <?php echo "$calculation" ?>
