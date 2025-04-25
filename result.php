21651f9c0ac7d08e261f340d1b3339c190b1d680
Shem Irekpita <Shem.irekpita@stu.ocsb.ca>
2025-04-25 15:10:33 +0000

    this is a nwe one

--- result.php
+++ result.php
@@ -1,7 +1,13 @@
 <?php
-	$fahrenheit = $_POST['fahrenheit'];
-	$Conversion =($fahrenheit - 32) * 5/9;
-	$Conversion = round($Conversion,1);
+	$Y1int = $_POST['Y1int'];
+	$Y2int = $_POST['Y2int'];
+	$X1int = $_POST['X1int'];
+	$X2int = $_POST['X2int'];
+
+	$slope = (Y2int - Y1int)/(X2int - X1int);
+	$calculation = Y2int - (slope * X2int)
+	$calculation = round($calculation;1)
 ?>
 <h3>Results:</h3>
-Volume of a sphere is <?php echo "$Conversion" ?>°F.
+Your slope is <?php echo "$slope" ?>
+Your Yint is <?php echo "$calculation" ?>
